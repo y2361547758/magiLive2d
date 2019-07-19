a1145afd4a6adaccb86ec39e4b7acc653db81edb
y2361547758 <2361547758@qq.com>
2019-07-20 01:20:14 +0800

    bugfix: zoom with px style cdoe

--- js/page.js
+++ js/page.js
@@ -1,11 +1,11 @@
 let page = 0;
 let models = null;
 let voices = null;
-let sl = $('#custom');
+let sl = $("#custom");
 let audio;
 
 function triggerF() { follow = !follow; sl.change(); }
-function resize() { init(parseInt($('#width').val()), parseInt($('#height').val())); sl.change(); }
+function resize() { init(parseInt($("#width").val()), parseInt($("#height").val())); sl.change(); }
 
 const downloadBlob = function(index) {
     return function(blob) {
@@ -28,37 +28,37 @@ function get(url, callback) {
 }
 
 $(document).ready(function() {
-    init(parseInt($('#width').val()), parseInt($('#height').val()));
+    init(parseInt($("#width").val()), parseInt($("#height").val()));
 
     get("/magica/resource/sound_native/voice/list.json", function (list) {voices = list});
     get("/magica/resource/image_native/live2d/list.json", function (list) {
         models = list
-        let sc = $('#char');
+        let sc = $("#char");
         sc.empty()
         for (let c in list) {
             char = list[c];
-            sc.append($('<option></option>').text(c + "-" + char.name).val(c));
+            sc.append($("<option></option>").text(c + "-" + char.name).val(c));
         }
         sc.val("");
 
         sc.change(function() {
             sl.empty();
-            if (sc.val() in list) for (let c in list[sc.val()]['models']) {
-                custom = list[sc.val()]['models'][c];
-                let o = $('<option></option>');
+            if (sc.val() in list) for (let c in list[sc.val()]["models"]) {
+                custom = list[sc.val()]["models"][c];
+                let o = $("<option></option>");
                 o.text(c + "-" + custom);
                 o.val(c);
                 sl.append(o);
             }
             sl.val("");
         })
-        $('#zoom').change(function() {
-            $('#Lzoom').text("Zoom: " + this.value + '%');
-            $(renderer.view).css("width", this.value + '%');
+        $("#zoom").change(function() {
+            $("#Lzoom").text("Zoom: " + this.value + "%");
+            $(renderer.view).css("width", this.value * $("#width").val() / 100 + "px");
         });
-        let se = $('#function > #g1 > select.exp');
-        let sm = $('#function > #g1 > select.motion');
-        let sv = $('#function > #g1 > select.voice');
+        let se = $("#function > #g1 > select.exp");
+        let sm = $("#function > #g1 > select.motion");
+        let sv = $("#function > #g1 > select.voice");
         sl.change(function() {
             let lastChild = null;
             while (lastChild = stage.children.shift()) { lastChild.destroy(); }
@@ -66,14 +66,14 @@ $(document).ready(function() {
                 se.empty();
                 for (let c in model.expressions) {
                     exp = model.expressions[c];
-                    se.append($('<option></option>').text(c).val(exp.name));
+                    se.append($("<option></option>").text(c).val(exp.name));
                 }
                 se.val("");
                 
                 sm.empty();
                 for (let c in model.motions.motion) {
                     exp = model.motions.motion[c];
-                    sm.append($('<option></option>').text(exp.name).val(c));
+                    sm.append($("<option></option>").text(exp.name).val(c));
                 }
                 sm.val("");
             });
@@ -85,23 +85,23 @@ $(document).ready(function() {
                 for (let i in char["00"]) list[parseInt(i)] = char["00"][i];
                 let custom = char[sl.val().slice(4)];
                 if (custom) for (let i in custom) list[parseInt(i)] = custom[i];
-                for (let c in list) { sv.append($('<option></option>').text(c).val(list[c])) }
+                for (let c in list) { sv.append($("<option></option>").text(c).val(list[c])) }
             }
             sv.val("");
         });
         se.change(function() { stage.children[0].model.setExpression(this.value) });
         sm.change(function() { stage.children[0].model.startMotion("motion", this.value) });
         sv.change(function() { 
-            audio = stage.children[0].model.playSound(this.value + ".mp3", '/magica/resource/sound_native/voice/');
+            audio = stage.children[0].model.playSound(this.value + ".mp3", "/magica/resource/sound_native/voice/");
         });
-        $('#function > #g1 > button.exp').click(function(){ se.change() });
-        $('#function > #g1 > button.motion').click(function(){ sm.change() });
-        $('#function > #g1 > button.replay').click(function(){
+        $("#function > #g1 > button.exp").click(function(){ se.change() });
+        $("#function > #g1 > button.motion").click(function(){ sm.change() });
+        $("#function > #g1 > button.replay").click(function(){
             sv.change();
             audio.load();
             audio.play();
         });
-        $('#function > #g1 > button.pause').click(function(){
+        $("#function > #g1 > button.pause").click(function(){
             if (audio.paused) audio.play();
             else audio.pause();
         });
